f7b92c4e68a40bdf18cc7b0a1b3f5fd84dc4a5fe
HaSeulLin <wjstiger@naver.com>
2023-05-25 10:19:47 +0900

    0525 파이어베이스 유저 정보 저장

--- react-firebase-store/src/page/FireStoreTest.jsx
+++ react-firebase-store/src/page/FireStoreTest.jsx
@@ -1,9 +1,11 @@
 import { addDoc, collection, getDocs } from 'firebase/firestore';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { db } from '../database/firebase'
 
 export default function FireStoreTest() {
+  // 파이어스토어에서 가져온 값을 출력
+  const [users, setUsers] = useState();
 
   // 사용자 문서 추가 (파이어스토어)
   const addDocData = async () => {
@@ -27,13 +29,17 @@ export default function FireStoreTest() {
       // getDocs를 통해서 컬렉션 안의 모든 문서 가져옴
       const querySnapshot = await getDocs(collection(db, "users"));
 
+      let dataArray = [];
       // forEach를 통해서 모든 문서값에 접근하여 원하는 값을 가져온다
       querySnapshot.forEach((doc) => {
         // doc.id와 doc.data()값을 리덕스에 저장하여
-        // 웹에서 사용
-      console.log(`${doc.id} => ${doc.data().first}`);
-      console.log(`${doc.id} => ${doc.data().born}`);
-    });
+        // 웹에서 사용 >> forEach의 모든 내용을 배열로 저장
+        console.log(`${doc.id} => ${doc.data().first}`);
+        
+        dataArray.push(doc.data());
+      });
+      // 값이 들어간 배열을 state에 넣어서 활용
+      setUsers(dataArray);
     }
     getData();
   }, []);
@@ -43,6 +49,14 @@ export default function FireStoreTest() {
       <h3>파이어스토어의 값을 추가, 가져옴 확인</h3>
       <p>users컬렉션 확인</p>
       <button onClick={addDocData}>버튼을 누르면 파이어스토어에 값 추가</button>
+      <br />
+      {
+        users && users.map((user, index)=>(
+          <div key={index}>
+            <p>{user.first}, {user.born}</p>
+          </div>
+        ))
+      }
     </div>
   )
 }
